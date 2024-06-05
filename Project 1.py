3bdd38a687a898b04843c31ec7b56c46a0f6865a
GHITESH15 <ganeshsundaram.m@gmail.com>
2024-06-05 07:49:49 -0700

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,10 @@
+# Project 1
+
+colour=input("enter the signal colour:")
+
+if colour=="red":
+    print("your vehicle should not move")
+elif colour=="green":
+    print("your vehicle can go")
+else:
+    print("your vehicle is going to stop")
